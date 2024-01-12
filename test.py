ca3f55d9b782251cff9b6baa07fc386e686f6904
adi1999 <adityadhookia@gmail.com>
2024-01-12 21:26:55 +0530

    test

--- test.py
+++ test.py
@@ -14,6 +14,8 @@ class Client:
     def __init__(self, entity: str, reports: List[ReportLimit]):
         self.entity = entity
         self.reports = reports
+
+
 def create_map(client: Client) -> Dict[str, List[str]]:
     result_map = {}
     
@@ -24,17 +26,37 @@ def create_map(client: Client) -> Dict[str, List[str]]:
         result_map[report_name] = lrd_codes
     
     return result_map
+    
 
+
+def detectanomaly( df:DataFrame ):
+    anomaly_model = onnxruntime.InferenceSession('anomaly_model.onnx')
+    test_features = df.drop('status', axis=1).values.astype('float32')
+    input_name = anomaly_model.get_inputs()[0].name
+    output_name = anomaly_model.get_outputs()[0].name
+    preds = anomaly_model.run([output_name], {input_name: test_features})
+    return preds
+
+def data_processing(data:DataFrame):
+    if(len(data)!=0):
+        print(data['data']['hardwareData'])
+        hardware_stats = get_hardware_stats(data['data']['hardwareData'].split(',')[0])
+        network_anomaly_data = get_network_anomaly_data(data['data']['networkData'])
+        sio.emit('model_hardware_output', hardware_stats)
+        sio.emit('network_anomaly_output', network_anomaly_data)
 
 
 def calculate_square(number):
     return number ** 2
+
 
 def calculate_probability(event_outcomes, total_outcomes):
     if total_outcomes == 0:
         return 0
     else:
         return event_outcomes / total_outcomes
+
+
 
 def calculate_factorial(n):
     if n == 0 or n == 1:
