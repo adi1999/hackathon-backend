69e893fb80fa443d49f20418ebcff52caad2850e
adi1999 <adityadhookia@gmail.com>
2024-01-13 14:51:52 +0530

    test

--- test.py
+++ test.py
@@ -12,3 +12,30 @@ def weighted_loss(original_loss_func, weights_list):
         loss = loss * weight_multiplier
         return loss
     return loss_func
+
+
+def weighted_cc(class_weights):
+    def loss(y_true, y_pred):
+        cc_loss = tf.keras.losses.categorical_crossentropy(y_true, y_pred)
+        return cc_los
+    return weighted_loss(loss, class_weights)
+
+
+def count_plus_mse_loss_channelwise2(loss_weights=(1, 1), dm_weight=100):
+    def loss(y_true, y_pred):
+        overall_loss = []
+        for channel in range(y_pred.shape[-1]):
+            pred_count = tf.round(tf.reduce_sum(y_pred[:, :, :, channel]) / dm_weight)
+            true_count = tf.round(tf.reduce_sum(y_true[:, :, :, channel]) / dm_weight)
+            count_loss = tf.square(tf.subtract(pred_count, true_count))
+            count_loss = tf.multiply(count_loss, loss_weights[0])  #(1, no_clas)
+            mse_loss = tf.square(tf.subtract(y_true[:, :, :, channel], y_pred[:, :, :, channel]))
+            mse_loss = tf.multiply(mse_loss, loss_weights[1])  # (320, 320)
+            overall_loss.append(tf.multiply(mse_loss, count_loss))
+        # overall_loss = tf.reduce_mean(overall_loss, axis=0)
+        overall_loss = tf.stack(overall_loss)
+        overall_loss = tf.transpose(overall_loss, perm=[1, 0, 2, 3])
+        overall_loss = tf.transpose(overall_loss, perm=[0, 3, 2, 1])
+        overall_loss = tf.transpose(overall_loss, perm=[0, 2, 1, 3])  # (320 ,320, no_classes)
+        return overall_loss
+    return loss
