25fca8703c040cdb4eee111b05858cc63afd50aa
adi1999 <adityadhookia@gmail.com>
2024-01-18 22:47:41 +0530

    test

--- test.py
+++ test.py
@@ -1,21 +1,14 @@
-def conv_bn_relu_x2(nb_filter, row, col, kernel_initializer='glorot_uniform', kernel_regularizers=None,
-                    bias_regularizers=None, dropout=False):
-    def f(input):
-        conv_a = Conv2D(nb_filter, (row, col),
-                        kernel_initializer=kernel_initializer, padding='same', use_bias=False,
-                        kernel_regularizer=kernel_regularizers,
-                        bias_regularizer=bias_regularizers, trainable=True)(input)
-        norm_a = BatchNormalization(trainable=True)(conv_a)
-        act_a = Activation(activation='relu')(norm_a)
-        if dropout:
-            act_a = Dropout(0.5)(act_a)
-        conv_b = Conv2D(nb_filter, (row, col),
-                        kernel_initializer=kernel_initializer, padding='same', use_bias=False,
-                        kernel_regularizer=kernel_regularizers,
-                        bias_regularizer=bias_regularizers, trainable=True)(act_a)
-        norm_b = BatchNormalization(trainable=True)(conv_b)
-        act_b = Activation(activation='relu')(norm_b)
-        if dropout:
-            act_b = Dropout(0.5)(act_b)
-        return act_b
-    return f
+def weighted_loss(original_loss_func, weights_list):
+    def loss_func(true, pred):
+        axis = -1
+        class_selectors = tf.keras.backend.cast(tf.keras.backend.argmax(true, axis=axis), tf.int32)
+        class_selectors = [tf.keras.backend.equal(i, class_selectors) for i in range(len(weights_list))]
+        class_selectors = [tf.keras.backend.cast(x, tf.keras.backend.floatx()) for x in class_selectors]
+        weights = [sel * w for sel, w in zip(class_selectors, weights_list)]
+        weight_multiplier = weights[0]
+        for i in range(1, len(weights)):
+            weight_multiplier = weight_multiplier + weights[i]
+        loss = original_loss_func(true, pred)
+        loss = loss * weight_multiplier
+        return loss
+    return loss_func
