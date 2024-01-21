9e4aba15e07d3c8e3b072ed1526bdeba4cd46664
adi1999 <adityadhookia@gmail.com>
2024-01-21 18:15:50 +0530

    sdgfs

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
-def weighted_loss(original_loss_func, weights_list):
-    def loss_func(true, pred):
-        axis = -1
-        class_selectors = tf.keras.backend.cast(tf.keras.backend.argmax(true, axis=axis), tf.int32)
-        class_selectors = [tf.keras.backend.equal(i, class_selectors) for i in range(len(weights_list))]
-        class_selectors = [tf.keras.backend.cast(x, tf.keras.backend.floatx()) for x in class_selectors]
-        weights = [sel * w for sel, w in zip(class_selectors, weights_list)]
-        weight_multiplier = weights[0]
-        for i in range(1, len(weights)):
-            weight_multiplier = weight_multiplier + weights[i]
-        loss = original_loss_func(true, pred)
-        loss = loss * weight_multiplier
+def jac_loss(class_weights=[1,20]):
+    def loss(y_true, y_pred):
+        smooth = 100
+        inter = tf.keras.backend.sum(tf.math.abs(y_true * y_pred), axis=-1)
+        sum_value = tf.keras.backend.sum(tf.math.abs(y_true) + tf.math.abs(y_pred), axis=-1)
+        jac_coeff = (inter + smooth)/(sum_value - inter + smooth)
+        return (1 - jac_coeff) * smooth
+    if class_weights:
+        return weighted_loss(loss, class_weights)
+    else:
         return loss
-    return loss_func
